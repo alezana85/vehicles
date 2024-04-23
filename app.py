d39bcf2456ea1b85237a4891411ace9cb20862df
alezana85 <alezana85@outlook.com>
2024-04-23 00:57:57 -0600

    last

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ build_histogram = st.checkbox('Construir un histograma')
 
 # contruir un Histograma con el boton con checkbox
 if build_histogram:
-    st.write('Creación un Histograma con los datos <img src="histogram_image.png" alt="Icono de ejemplo" style="width:100px;">', unsafe_allow_html=True)    
+    st.write('Creación un Histograma con los datos <img src="histogram_image.png" style="width:100px;">', unsafe_allow_html=True)    
     fig = px.histogram(car_data, x='odometer', color_discrete_sequence=['#0E0E52', '#3943B7', '#78C0E0'])
     fig.show()
 
 if buil_scatter:
-    st.write('Crea un grafico de Dispersion con los datos <img src="scatter_image.png" alt="Icono de ejemplo" style="width:100px;">', unsafe_allow_html=True)    
+    st.write('Crea un grafico de Dispersion con los datos <img src="scatter_image.png" style="width:100px;">', unsafe_allow_html=True)    
     fig = px.scatter(car_data, x="odometer", y="price", color='model')
     fig.show()
     
