e0b859b2b5fd941f0058025d0abcb149cfa3c49e
alezana85 <alezana85@outlook.com>
2024-04-23 02:14:03 -0600

    now_the_last_commit

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ car_data = pd.read_csv('vehicles_us.csv')
 header = st.header('Datos de Anuncios de Ventas de Autos')
 build_scatter = st.checkbox('Crear Grafico de Dispersion')
 build_histogram = st.checkbox('Construir un histograma')
-hist_button = st.button('Construir Histograma')
 
 # contruir un Histograma con el boton con checkbox
-if hist_button:
+if build_histogram:
     st.write('Creación un Histograma con los datos <img src="histogram_image.png" style="width:100px;">', unsafe_allow_html=True)    
     fig = px.histogram(car_data, x='odometer', color_discrete_sequence=['#0E0E52', '#3943B7', '#78C0E0'])
     fig.show()
