61b0f5f4c00e9d7528350883301e57ad870fa066
alezana85 <alezana85@outlook.com>
2024-04-23 01:57:40 -0600

    commit_1

--- app.py
+++ app.py
@@ -4,16 +4,17 @@ import plotly_express as px
  
 car_data = pd.read_csv('vehicles_us.csv')
 header = st.header('Datos de Anuncios de Ventas de Autos')
-buil_scatter = st.checkbox('Crear Grafico de Dispersion')
+build_scatter = st.checkbox('Crear Grafico de Dispersion')
 build_histogram = st.checkbox('Construir un histograma')
+hist_button = st.button('Construir Histograma')
 
 # contruir un Histograma con el boton con checkbox
-if build_histogram:
+if hist_button:
     st.write('Creación un Histograma con los datos <img src="histogram_image.png" style="width:100px;">', unsafe_allow_html=True)    
     fig = px.histogram(car_data, x='odometer', color_discrete_sequence=['#0E0E52', '#3943B7', '#78C0E0'])
     fig.show()
 
-if buil_scatter:
+if build_scatter:
     st.write('Crea un grafico de Dispersion con los datos <img src="scatter_image.png" style="width:100px;">', unsafe_allow_html=True)    
     fig = px.scatter(car_data, x="odometer", y="price", color='model')
     fig.show()
